47c9f538b2422534fd23587c92d3668b42a601f2
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-04-05 09:13:27 +0100

    Used eslint to fix indentation issues in models/comment.js file

--- models/comment.js
+++ models/comment.js
@@ -1,13 +1,14 @@
 import dbClient from '../utils/db';
 
 const commentSchema = new dbClient.mongoose.Schema(
-    {
-	username: String,
-	text: String,
-	date: {
-	    type: Date,
-	    default: Date.now
-	}
-    });
+  {
+    username: String,
+    text: String,
+    date: {
+	type: Date,
+	default: Date.now,
+    },
+  },
+);
 
 module.exports = dbClient.mongoose.model('Comment', commentSchema);
