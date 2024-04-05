23a31bac874c283d68c62b3538c172d79a2c9b6d
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-04-05 09:05:18 +0100

    Used eslint to fix indentation issues in utils/redis.js file

--- utils/redis.js
+++ utils/redis.js
@@ -2,35 +2,35 @@ import { createClient } from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-    constructor() {
-	this.client = createClient();
+  constructor() {
+    this.client = createClient();
 
-	this.client.on('error', (err) => {
-	    console.log(new Error(err));
-	}).on('connect', () => {
-	    console.log(`Redis is up and active`);
-	});
-    }
+    this.client.on('error', (err) => {
+	console.log(new Error(err));
+    }).on('connect', () => {
+	console.log('Redis is up and active');
+    });
+  }
 
-    isActive() {
-	return this.client.connected;
-    }
+  isActive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
-	const get = promisify(this.client.get).bind(this.client);
-	const value = await get(key);
-	return value;
-    }
+  async get(key) {
+    const get = promisify(this.client.get).bind(this.client);
+    const value = await get(key);
+    return value;
+  }
 
-    async set(key, value, expiration) {
-	const setter = promisify(this.client.set).bind(this.client);
-	await setter(key, value, 'EX', expiration)
-    }
+  async set(key, value, expiration) {
+    const setter = promisify(this.client.set).bind(this.client);
+    await setter(key, value, 'EX', expiration);
+  }
 
-    async del(key) {
-	const deletion = promisify(this.client.del).bind(this.client);
-	await deletion(key);
-    }
+  async del(key) {
+    const deletion = promisify(this.client.del).bind(this.client);
+    await deletion(key);
+  }
 }
 
 const redisClient = new RedisClient();
