8f1cc81494170e5a97631f4e3b04e5933b7014b3
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-04-05 09:08:28 +0100

    Used eslint to fix indentation issues in utils/db.js file

--- utils/db.js
+++ utils/db.js
@@ -5,22 +5,20 @@ const PORT = process.env.PORT || 27017;
 const DB = process.env.DB || 'myTeenDb';
 
 class DbClient {
-    constructor() {
-	this.mongoose = mongoose;
-	this.mongoose.connect(`mongodb://${HOST}:${PORT}/${DB}`);
+  constructor() {
+    this.mongoose = mongoose;
+    this.mongoose.connect(`mongodb://${HOST}:${PORT}/${DB}`);
 
-	this.mongoose.connection.on('connected', () => {
-	    console.log('Database is Set');
-	}).on('error', (err) => {
-	    console.log(new Error(err));
-	});
-	
-    
-    }
+    this.mongoose.connection.on('connected', () => {
+	console.log('Database is Set');
+    }).on('error', (err) => {
+	console.log(new Error(err));
+    });
+  }
 
-    isActive() {
-	return this.mongoose.connection.readyState;
-    }
+  isActive() {
+    return this.mongoose.connection.readyState;
+  }
 }
 
 const dbClient = new DbClient();
