d7e79294994c2ce9779307892b508006467a9ba2
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-04-05 09:09:50 +0100

    Used eslint to fix indentation issues in models/post.js file

--- models/post.js
+++ models/post.js
@@ -2,18 +2,19 @@ import dbClient from '../utils/db';
 import Comment from './comment';
 
 const postSchema = new dbClient.mongoose.Schema(
-    {
-	username: String,
-	body: String,
-	likes: {
-	    type: Number,
-	    default: 0,
-	},
-	comments: [Comment.schema],
-	date: {
-	    type: Date,
-	    default: Date.now
-	}
-    });
+  {
+    username: String,
+    body: String,
+    likes: {
+	type: Number,
+	default: 0,
+    },
+    comments: [Comment.schema],
+    date: {
+	type: Date,
+	default: Date.now,
+    },
+  },
+);
 
 module.exports = dbClient.mongoose.model('Post', postSchema);
