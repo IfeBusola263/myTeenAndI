293b557d88a63e1bf6ae4340b9d88f1896a7740d
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-04-05 09:12:37 +0100

    Used eslint to fix indentation issues in models/user.js file

--- models/user.js
+++ models/user.js
@@ -1,57 +1,56 @@
+import { isEmail } from 'validator';
 import dbClient from '../utils/db';
 import Post from './post';
-import { isEmail } from 'validator';
 
 const userSchema = new dbClient.mongoose.Schema(
-    {
-	name: String,
-	email: {
-	    type: String,
-	    required: true,
-	    unique: true,
-	    validate: [isEmail, "Please enter a valid email address"] 
-	},
-	username: {
-	    type: String,
-	    required: true,
-	    unique: true
-	},
-	bio: {
-	    type: String,
-	    default: ""
-	},
-	createdAt: {
-	    type: Date,
-	    immutable: true,
-	    default: Date.now
-	},
-	updatedAt: {
-	    type: Date,
-	    immutable: true,
-	    default: Date.now,
-	},
-	password: {
-	    type: String,
-	    required: true,
-	    unique: true
-	},
-	phoneNumber:
-	{
-	    type: Number,
-	    unique: true
-	},
-	posts: [Post.schema]
-    });
+  {
+    name: String,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [isEmail, 'Please enter a valid email address'],
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    bio: {
+      type: String,
+      default: '',
+    },
+    createdAt: {
+      type: Date,
+      immutable: true,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      immutable: true,
+      default: Date.now,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phoneNumber: {
+      type: Number,
+      unique: true,
+    },
+    posts: [Post.schema],
+  },
+);
 
-userSchema.pre('remove', function (next) {
-    console.log(`${this.name} is about to be deleted!`);
-    next();
+userSchema.pre('remove', function deleInfo(next) {
+  console.log(`${this.name} is about to be deleted!`);
+  next();
 });
 
-userSchema.post('findOneAndDelete', function (doc, next) {
-    console.log(`${doc.name} has been deleted, We should find out why.`)
-    next();
-})
-
+userSchema.post('findOneAndDelete', (doc, next) => {
+  console.log(`${doc.name} has been deleted, We should find out why.`);
+  next();
+});
 
 module.exports = dbClient.mongoose.model('User', userSchema);
