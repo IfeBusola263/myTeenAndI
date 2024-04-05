6cfeaaa968cc486c7d3529702aa3866f160d0b27
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-04-05 09:02:58 +0100

    Removed the unused variables in the Server.js File

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 import express from 'express';
-import router from './routes/index';
 import cors from 'cors';
+import router from './routes/index';
 
 const PORT = 3000;
-const HOST = 'localhost';
+// const HOST = 'localhost';
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 app.use('/', router);
 
-app.listen(PORT, ()=> {
-    console.log(`SERVER is running on PORT ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`SERVER is running on PORT ${PORT}`);
 });
